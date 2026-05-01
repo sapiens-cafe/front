957113c073f7e1f17bb2383d313adf1992e0990c
Najib Slassi <mohamed-najib.slassi@mila.fr>
2026-05-01 15:14:01 +0200

    fix email

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -5,7 +5,7 @@ export async function POST(request: Request) {
     const requestBody = await request.json();
     const { data, error } = await resend.emails.send({
       from: 'onboarding@resend.dev',
-      to: 'info@sapienscafe.com',
+      to: 'info@sapienscafe.fr',
       subject: 'Nouveau message contact sapiens.com',
       html: requestBody.html,
     });
